8ccb1e9ce4b85c3bc257bd71fe864a016b8db945
Joel Klabo <joelklabo@gmail.com>
2023-02-01 22:32:00 -0800

    Fix Vertical Icon Spacing for Small Screens

--- components/header.js
+++ components/header.js
@@ -11,14 +11,14 @@ export default function Header() {
 						<span className="text-2xl">klabo.blog</span>
 					</Link>
 				</div>
-				<div className="flex gap-6">
+				<div className="flex gap-6 pt-1 sm:pt-0">
 					<Link href={"/tip"}>
 						<div className="flex items-center">
 							<FaBitcoin /> <span className="pl-2 pr-2">+</span> <BsFillLightningChargeFill />
 						</div>
 					</Link>
 				</div>
-				<div className="flex gap-6">
+				<div className="flex gap-6 pt-2 sm:pt-0">
 					<Link href="nostr:2f4fa408d85b962d1fe717daae148a4c98424ab2e10c7dd11927e101ed3257b2">
 						<GiOstrich />
 					</Link>
