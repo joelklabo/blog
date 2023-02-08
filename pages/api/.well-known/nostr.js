8ea5290919940d8c7ef5600b96481933172d2a9c
Joel Klabo <joelklabo@gmail.com>
2023-02-08 10:37:27 -0800

    Add _ NIP-05

--- pages/api/.well-known/nostr.js
+++ pages/api/.well-known/nostr.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
 	const response = {"names":
 		{
-			"joel": "2f4fa408d85b962d1fe717daae148a4c98424ab2e10c7dd11927e101ed3257b2"
+			"joel": "2f4fa408d85b962d1fe717daae148a4c98424ab2e10c7dd11927e101ed3257b2",
+			"_": "2f4fa408d85b962d1fe717daae148a4c98424ab2e10c7dd11927e101ed3257b2" 
 		}
 	}
 	res.status(200).json(response)
