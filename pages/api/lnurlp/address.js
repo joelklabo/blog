5e02f63ee65a06de37fb9eb7388066ad555fa12c
Joel Klabo <joelklabo@gmail.com>
2023-02-13 18:34:21 -0800

    Fix Typo

--- pages/api/lnurlp/address.js
+++ pages/api/lnurlp/address.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
 	const { user } = req.query
 	const response = {
-		"callback": "https://klabo.blog/api/lurlp/invoice",
+		"callback": "https://klabo.blog/api/lnurlp/invoice",
 		"maxSendable": 400000000,
 		"minSendable": 1000,
 		"metadata": "[[\"text/identifier\",\"joel@klabo.blog\"],[\"text/plain\",\"Send sats to joel@klabo.blog\"]]",	
