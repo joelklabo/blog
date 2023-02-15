c465d3450ab31282d5238e12071c3696599a3a1d
Joel Klabo <joelklabo@gmail.com>
2023-02-14 16:04:30 -0800

    Adding Lightning Address Handling

--- pages/api/lnurlp/address.js
+++ pages/api/lnurlp/address.js
@@ -1,6 +1,9 @@
 import NextCors from 'nextjs-cors'
+import LightningAddress from '@/lib/lightningAddress'
 
 export default async function handler(req, res) {
+
+	const lnAddress = new LightningAddress();
 
 	await NextCors(req, res, {
 		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
@@ -8,18 +11,5 @@ export default async function handler(req, res) {
 		optionsSuccessStatus: 200
 	})
 
-	const { user } = req.query
-
-	const response = {
-		"callback": "https://klabo.blog/api/lnurlp/invoice",
-		"maxSendable": 400000000,
-		"minSendable": 1000,
-		"metadata": "[[\"text/identifier\",\"joel@klabo.blog\"],[\"text/plain\",\"Send sats to joel@klabo.blog\"]]",	
-		"commentAllowed": 500,
-		"tag": "payRequest",
-		"nostrPubKey": "2f4fa408d85b962d1fe717daae148a4c98424ab2e10c7dd11927e101ed3257b2",
-		"allowsNostr": true
-	}
-	
-	res.status(200).json(response)
+	res.status(200).json(lnAddress.initialResponse);
 }
--- pages/api/lnurlp/invoice.js
+++ pages/api/lnurlp/invoice.js
@@ -1,29 +1,23 @@
-import LNBits from 'lnbits';
 import NextCors from 'nextjs-cors';
+import LightningAddress from '@/lib/lightningAddress';
 
-const { wallet } = LNBits({
-  adminKey: process.env.LNBITS_ADMIN_KEY,
-  invoiceReadKey: process.env.LNBITS_INVOICE_KEY,
-  endpoint: 'https://lnbits.klabo.blog',
-});
 
 export default async function handler(req, res) {
+
+	const lnAddress = new LightningAddress();
 
 	await NextCors(req, res, {
 		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
 		origin: '*',
 		optionsSuccessStatus: 200
 	})
-	console.log(req.query)
-	const newInvoice = await wallet.createInvoice({
-		amount: req.query.amount / 1000,
-		memo: `Description: ${Date.now()}`,
-		out: false,
-	})
-	console.log(newInvoice)
-	const repsonse = {
-		"pr": newInvoice.payment_request,
-		"routes": []
-	}
-	res.status(200).json(repsonse)
+	
+	lnAddress.invoice(req.query.amount, (newInvoice) => {
+		console.log(newInvoice);
+		const response = {
+			"pr": newInvoice.bolt11,
+			"routes": []
+		}
+		res.status(200).json(response)
+	});
 }
