0984c513872d75865212bf168634e5ef4770f215
Joel Klabo <joelklabo@gmail.com>
2023-02-14 21:03:12 -0800

    Amount is not a number

--- pages/api/lnurlp/invoice.js
+++ pages/api/lnurlp/invoice.js
@@ -12,19 +12,15 @@ export default async function handler(req, res) {
 		optionsSuccessStatus: 200
 	})
 	
-	if (typeof req.query.amount === 'number') {
-		lnAddress.invoice(req.query.amount, (response) => {
-			if (typeof response.result.bolt11 === 'string') {
-				const payload = {
-					"pr": response.result.bolt11,
-					"routes": []
-				}
-				res.status(200).json(payload)
-			} else {
-				res.status(400).json({ error: "Failed to create invoice" });
+	lnAddress.invoice(req.query.amount, (response) => {
+		if (typeof response.result.bolt11 === 'string') {
+			const payload = {
+				"pr": response.result.bolt11,
+				"routes": []
 			}
-		});
-	} else {
-		res.status(400).json({ error: "Amount is required" });
-	}
+			res.status(200).json(payload)
+		} else {
+			res.status(400).json({ error: "Failed to create invoice" });
+		}
+	});
 }
