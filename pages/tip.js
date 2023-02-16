4a32f2ebc8d06c4c6b3583d27e5adb9c980af91d
Joel <joelklabo@gmail.com>
2023-02-16 21:01:22 +0000

    Use PaidInvoices Component

--- pages/tip.js
+++ pages/tip.js
@@ -1,9 +1,9 @@
 import Layout from "@/components/layout";
-import PaidInvoice from "@/components/paidInvoice";
+import PaidInvoices from "@/components/paidInvoices";
 import TipFooter from "@/components/tipFooter";
 
 export async function getServerSideProps() {
-	const res = await fetch('https://beta.klabo.blog/api/lightning/pays');
+	const res = await fetch(`${process.env.BASE_URL}/api/lightning/pays`);
 	const result = await res.json();
 	return {
 		props: {
@@ -17,28 +17,7 @@ export default function Tip({ paidInvoices}) {
 		<div class="overflow-hidden">
 			<Layout>
 				<TipFooter />
-				<div class="overflow-auto">
-    		<table class="table-fixed w-full text-sm text-left text-gray-500">
-      		<thead class="text-xs text-gray-700 uppercase bg-gray-100">
-          	<tr>
-          	    <th scope="col" class="px-6 py-3">
-          	      Date 
-          	    </th>
-          	    <th scope="col" class="px-6 py-3">
-									Message
-          	    </th>
-          	    <th scope="col" class="px-6 py-3 text-right">
-									Sats
-          	    </th>
-          	</tr>
-      		</thead>
-     			<tbody>
-						{paidInvoices.map((paidInvoice) => (
-							<PaidInvoice paidInvoice={paidInvoice} key={paidInvoice.payment_hash} />
-						))};
-					</tbody>
-					</table>
-				</div>
+				<PaidInvoices paidInvoices={paidInvoices} />
 			</Layout>
 		</div>
 	)
