1effc5c01462a83a8df48cd05b033c6fe3798621
Joel <joelklabo@gmail.com>
2023-02-22 00:37:50 +0000

    Move Metadata to Own Object

--- pages/api/lightning/pays.js
+++ pages/api/lightning/pays.js
@@ -1,8 +1,10 @@
 import NextCors from "nextjs-cors";
 import LightningRPC from "@/lib/lightning/rpc";
+import Metadata from "@/lib/lightning/metadata";
 
 export default async function handler(req, res) {
 	const rpc = new LightningRPC(`${process.env.LIGHTNING_RPC}`);
+	const metadata = new Metadata();
 
 	await NextCors(req, res, {
 		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
@@ -13,8 +15,15 @@ export default async function handler(req, res) {
 	await rpc.invoices().then((invoices) => {
 		const paidInvoices = invoices.filter((invoice) => {
 			return invoice.status === 'paid';
-		}).reverse();
-		res.status(200).json({ paidInvoices: paidInvoices });
+		})
+		.map((invoice) => {
+			if (invoice.description === metadata.metadataString) {
+				invoice.description = "⚡️ Tip";
+			}
+			return invoice;
+		})
+		.reverse();
+		res.status(200).json({ paidInvoices: paidInvoices});
 	}).catch((error) => {
 		res.status(400).json({ error: "Failed to get invoices" });
 	});
