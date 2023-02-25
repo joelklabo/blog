e90ed4bfb66128320535e7210d07e01c44621c5c
Joel <joelklabo@gmail.com>
2023-02-25 05:43:56 +0000

    Format Tip Types

--- components/paidInvoice.js
+++ components/paidInvoice.js
@@ -1,3 +1,4 @@
+import { DefaultType, KeysendType, NostrType, TipType } from "@/models/paidInvoice";
 import RelativeDate from "./relativeDate";
 
 export default function PaidInvoice({paidInvoice}) {
@@ -7,7 +8,18 @@ export default function PaidInvoice({paidInvoice}) {
 				<RelativeDate dateString={paidInvoice.date} />
 			</th>
 			<td class="px-6 py-4 truncate">
-				{paidInvoice.description}
+			{(() => {
+        switch (paidInvoice.type) {
+          case NostrType:
+            return <NostrMessage paidInvoice={paidInvoice} /> 
+          case TipType:
+            return <TipMessage paidInvoice={paidInvoice} />
+          case KeysendType:
+            return <KeysendMessage paidInvoice={paidInvoice} />
+          default:
+            return <DefaultMessage paidInvoice={paidInvoice} /> 
+        }
+      })()}
 			</td>
 			<td class="px-6 py-4 text-right">
 				{paidInvoice.amount}
@@ -15,3 +27,39 @@ export default function PaidInvoice({paidInvoice}) {
 		</tr>
 	)
 }
+
+function NostrMessage({paidInvoice}) {
+	return (
+		<p>⚡️ Zap from {paidInvoice.pubkey}</p>
+	)
+};
+
+function TipMessage({paidInvoice}) {
+	return (
+		<p>⚡️ Tip</p>
+	)
+};
+
+function KeysendMessage({paidInvoice}) {
+	let message = paidInvoice.message;
+	
+	function makeMessage(message) {
+		if (paidInvoice.message.includes("keysend:")) {
+			return paidInvoice.message.split("keysend:")[1];
+		}
+		if (paidInvoice.message.includes("keysend")) {
+			return paidInvoice.message.split("keysend")[1];
+		}
+		return message;
+	}
+	
+	return (
+		<p>🔑 {makeMessage(message)}</p>
+	)
+};
+
+function DefaultMessage({paidInvoice}) {
+	return (
+		<p>💰 {paidInvoice.message}</p>
+	)
+};
